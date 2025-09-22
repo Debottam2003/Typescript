da2f51d9e49c22306275170dad733ed78bad26ce
Debottam Kar <debottamkar2003@gmail.com>
2025-09-22 19:47:47 +0530

    typescript for react

--- TSReact/TS/src/App.tsx
+++ TSReact/TS/src/App.tsx
@@ -1,8 +1,12 @@
-function App() {
+import { useState } from "react"
 
+const App: React.FC = function () {
+  const [data, setData] = useState<number>(0);
   return (
     <h2>
       Typescript for react
+      <h2>{data}</h2>
+      <button onClick={() => setData(data + 1)} >This is a button</button>
     </h2>
   )
 }
