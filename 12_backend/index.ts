0210408d4933da1b797152913166b538e8e962b4
Debottam Kar <debottamkar2003@gmail.com>
2025-10-11 23:27:53 +0530

    fixes

--- 12_backend/index.ts
+++ 12_backend/index.ts
@@ -1,14 +1,26 @@
 console.log("Hello via Bun!");
 
 function users() {
-  console.log("this is /users route");
+  console.log("this is /users route handler");
 }
 
-let middlewareStack = {
+let middlewareStack: { [key: string]: () => void } = {
   "/users": users,
   "/login": () => {
-    console.log("this is /login route");
+    console.log("this is /login route handler");
   },
 };
 
-middlewareStack["/users"]();
+middlewareStack["/users"]!();
+
+console.log(middlewareStack);
+
+function add(pathname: string, func: () => void) {
+  middlewareStack[pathname] = func;
+}
+
+add("/profile", () => {
+  console.log("this is the /profile route handler");
+});
+
+console.log(middlewareStack);
