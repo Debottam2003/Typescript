7037e873f7bd86348f1b950e4c59caf487b051f1
Debottamkar2003.com <debottamkar2003@gmail.com>
2025-12-07 13:08:26 +0530

    fixes

--- copyconstructor.ts
+++ copyconstructor.ts
@@ -2,19 +2,19 @@ console.log("This is copy constructor");
 let print = console.log;
 class Node {
 
-  constructor(x,y) {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
   }
 
   copy_constructor(node: Node) {
-   this.x = node.x;
-   this.y = node.y;
+    this.x = node.x;
+    this.y = node.y;
   }
 }
 
-let n1: Node = new Node(5,5);
-let n2: Node = new Node(10,10);
+let n1: Node = new Node(5, 5);
+let n2: Node = new Node(10, 10);
 print(n1);
 print(n2);
 
